1d7b4f9ebe008cc4b75b3e2a2a3900d23b8b537d
SudhirPawar123 <sudhipawar329@gmail.com>
2024-10-01 12:29:49 +0530

    we added states ,initialising using constructor and length() function

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -1,6 +1,18 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class LineComparison {
+    private double x1, y1, x2, y2;
+
+    public LineComparison(double x1, double y1, double x2, double y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    public double length() {
+        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+    }
     public static void main(String[] args) {
         System.out.printf("Welcome to Line Comparison Computation Problem..!");
     }
