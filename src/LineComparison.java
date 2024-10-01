8758c361be38e70af885da1bc81c6780c81ae18f
SudhirPawar123 <sudhipawar329@gmail.com>
2024-10-01 12:34:19 +0530

    overrided equals() function,checking the when lines are equal

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -9,9 +9,17 @@ public class LineComparison {
         this.x2 = x2;
         this.y2 = y2;
     }
-
     public double length() {
         return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        LineComparison line = (LineComparison) obj;
+
+        return (Double.compare(line.length(), this.length()) == 0);
     }
     public static void main(String[] args) {
         System.out.printf("Welcome to Line Comparison Computation Problem..!");
