ea81ab546408f243164115ae2abdc14c5b542c30
SudhirPawar123 <sudhipawar329@gmail.com>
2024-10-01 12:23:47 +0530

    Welcome message

---
+++ src/LineComparison.java
@@ -0,0 +1,7 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.printf("Welcome to Line Comparison Computation Problem..!");
+    }
+}
